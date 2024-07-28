2e981bec027daaa0258d0da19c1c5a2d6c090f56
Jimmy <jabez007@users.noreply.github.com>
2024-07-28 11:56:49 -0500

    nested for loops

--- main.tf
+++ main.tf
@@ -8,15 +8,14 @@ data "github_repositories" "my_topics" {
 
 resource "github_repository_environment" "environments" {
   for_each = {
-    for repo in data.github_repositories.my_topics.results : repo.name => repo.name
+    for repo in data.github_repositories.my_topics : repo.name => [
+      for env in local.environments : {
+        repository  = repo.name
+        environment = env
+      }
+    ]
   }
 
-  repository = each.value
-
-  dynamic "environment" {
-    for_each = local.environments
-    content {
-      name = environment.value
-    }
-  }
+  repository = each.value.repository
+  environment = each.value.environment
 }
